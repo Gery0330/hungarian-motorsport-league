0eac442ecd351f2e27065fa7ced3fced30c0aa6b
Gery0330 <v.gergo0330@gmail.com>
2026-03-04 18:04:28 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const supabaseUrl = "https://mdhbillpmxvsesjrcsmf.supabase.co";
-const supabaseKey = "IDE_JÖN_A_SUPABASE_PUBLISHABLE_KEY";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1kaGJpbGxwbXh2c2VzanJjc21mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE1MzE1OTAsImV4cCI6MjA4NzEwNzU5MH0.QGQ_AnbXN0fzNKdalQMdy4Cw1tmWU_gB0-usqeFxil0";
 
 window.supabaseClient = window.supabase.createClient(
   supabaseUrl,
